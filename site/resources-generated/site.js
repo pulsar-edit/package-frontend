b1e7762e6f0ea502db1c0b5ea9924c2f727aa311
confused-Techie <dev@lhbasics.com>
2025-11-15 18:34:53 -0800

    Handle advanced search options, make advanced sidebar toggleable

--- site/resources-generated/site.js
+++ site/resources-generated/site.js
@@ -120,7 +120,6 @@ const AccountActions = {
 
     // Set the "Sign In" to be "Sign Out"
     const loginLink = Array.from(headerLinks).find(i => i.href === `${document.location.origin}/login`);
-    console.log(headerLinks);
     loginLink.innerHTML = "Sign Out";
     loginLink.href = "/logout";
 
@@ -203,8 +202,143 @@ const AccountActions = {
   }
 };
 
+class AdvancedSearch {
+  /**
+  * The advanced search form is a secondary form element.
+  * This means by default any values there are not associated with the proper
+  * 'search' form during the submit event.
+  * But we can assign each individual input to the 'search' form at will.
+  * Except we **must** ensure to not assign any empty values or else those can
+  * cause a failed search, such as search for packages that provide the service '',
+  * since none do, nothing would be returned.
+  * So that means we can't just assign every input the 'form="search"' attribute,
+  * and we have to do it dynamically.
+  */
+  constructor() {
+    // Opts contains all advanced search options matching how they are specified in the document
+    this.opts = [ "sort", "direction", "fileExtension", "serviceType", "service", "serviceVersion", "owner" ];
+    this.forms = {};
+    this.advancedOptsSet = false; // Knowing if any advanced parameters have been set
+  }
+
+  setup() {
+    for (const opt of this.opts) {
+      this.forms[opt] = {
+        form: document.getElementById(`search__${opt}`)
+      };
+
+      this.forms[opt].form.addEventListener("input", this.changed.bind(this));
+    }
+
+    this.applyDefaults();
+    this.applyParams();
+    this.setupControlBtn();
+  }
+
+  changed(event) {
+    if (event.type === "input" && event.target.form !== "search") {
+      // Assign the input to the 'search' form to associate it's value to that form element
+      event.target.setAttribute("form", "search");
+    }
+
+    // But if it's a text value that's now empty, we **must** make sure to remove the association
+    if (event.target.type === "text" && event.target.value == "" && event.target.form === "search") {
+      event.target.removeAttribute("form");
+    }
+  }
+
+  applyDefaults() {
+    // Apply the default values to the advanced search forms
+
+    // Default values are only for some items
+    document.getElementById("search__sort--downloads").checked = true;
+    document.getElementById("search__direction--desc").checked = true;
+  }
+
+  applyParams() {
+    // Apply the paramaters from the URL to the advanced search forms
+    const urlParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlParams.entries());
+
+    for (const param in params) {
+      switch(param) {
+        case "sort":
+          document.querySelector(`input[value="${params[param]}"]`).checked = true;
+          this.advancedOptsSet = true;
+          break;
+        case "direction":
+          document.querySelector(`input[value="${params[param]}"]`).checked = true;
+          this.advancedOptsSet = true;
+          break;
+        case "fileExtension":
+          document.getElementById("search__fileExtension--input").value = params[param];
+          this.advancedOptsSet = true;
+          break;
+        case "serviceType":
+          document.querySelector(`input[value="${params[param]}"]`).checked = true;
+          this.advancedOptsSet = true;
+          break;
+        case "service":
+          document.getElementById("search__service--input").value = params[param];
+          this.advancedOptsSet = true;
+          break;
+        case "serviceVersion":
+          document.getElementById("search__serviceVersion--input").value = params[param];
+          this.advancedOptsSet = true;
+          break;
+        case "owner":
+          document.getElementById("search__owner--input").value = params[param];
+          this.advancedOptsSet = true;
+          break;
+      }
+    }
+  }
+
+  setupControlBtn() {
+    // Setup the advanced search button that controls expanding or collapsing
+    // the advanced search sidebar
+    document.getElementById("advanced-search-control").addEventListener("click", this.toggleControlBtn);
+
+    if (this.advancedOptsSet) {
+      // This means the previous search query was an advanced one, and the advanced options
+      // have been modified from the default
+      // We will want to immediately toggle the advanced sidebar from the default of
+      // disabled to enabled
+      this.toggleControlBtn();
+    }
+  }
+
+  toggleControlBtn() {
+    // Toggle the advanced search button
+    const btn = document.getElementById("advanced-search-control");
+    const sidebar = document.getElementById("advanced-search-sidebar");
+    const enabled = (btn.dataset.enabled.toLowerCase() == "true");
+    // The dataset value will be set here, so if it's enabled we know the user
+    // intends to disable it & vice versa
+
+    if (enabled) {
+      // It's enabled, the user intends to disable it
+      btn.classList.remove("icon-jump-right");
+      btn.classList.add("icon-jump-left");
+      sidebar.classList.add("hidden");
+      btn.dataset.enabled = "false";
+    } else {
+      // It's disabled, the user intends to enable it
+      btn.classList.remove("icon-jump-left");
+      btn.classList.add("icon-jump-right");
+      sidebar.classList.remove("hidden");
+      btn.dataset.enabled = "true";
+    }
+  }
+}
+
 window.onload = () => {
   AccountActions.setup();
   ThemeSwitcher.setup();
   CopyToClipboard.setup();
+
+  if (document.getElementById("advanced-search-sidebar")) {
+    const advancedSearch = new AdvancedSearch();
+    advancedSearch.setup();
+  }
 };
