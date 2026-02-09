9a98f2a1df907111f627b509e4aa17b1626dcbeb
confused-Techie <dev@lhbasics.com>
2026-02-09 01:10:43 -0800

    Use RFC9457 problem details specification

--- microservices/search/index.js
+++ microservices/search/index.js
@@ -10,11 +10,6 @@ const DOMAIN_MAP = {
 };
 const INDEXES = {}; // Instances of the search class w/ domain as the key
 
-app.get("/file", async (req, res) => {
-  const path = require("path");
-  res.sendFile(path.join(__dirname, "search-index.jsonl"));
-});
-
 app.post("/reindex/:domain", async (req, res) => {
   // Endpoint to trigger a re-index of content for a given sub-domain
   let domain = req.params.domain;
@@ -22,7 +17,11 @@ app.post("/reindex/:domain", async (req, res) => {
 
   if (!domain) {
     // We don't support whatever domain was provided
-    res.status(400).json({ err: "The Domain provided is not supported." });
+    res.status(400).append("Content-Type", "application/problem+json").json({
+      type: "https://search.pulsar-edit.dev/problems/unsupported-domain",
+      status: 400,
+      title: "The Domain provided is not supported."
+    });
     return;
   }
 
@@ -31,13 +30,28 @@ app.post("/reindex/:domain", async (req, res) => {
     await idx.reindex();
   } catch(err) {
     console.error(err);
-    res.status(500).json({ err: err.toString() });
+
+    if (err.toString() === "The data stream is undefined!") {
+      res.status(500).append("Content-Type", "application/problem+json").json({
+        type: "about:blank",
+        status: 500,
+        title: "The data stream location couldn't be identified.",
+        detail: err.toString()
+      });
+    } else {
+      res.status(500).append("Content-Type", "application/problem+json").json({
+        type: "about:blank",
+        status: 500,
+        title: "An unexpected problem occurred when attempting to reindex this domain.",
+        detail: err.toString()
+      });
+    }
     return;
   }
 
   INDEXES[domain] = idx;
 
-  res.status(201).json({ message: "Successfully reindexed" });
+  res.status(201).send();
 
   // After we have returned our response to the user, save our new index
   INDEXES[domain].save();
@@ -50,7 +64,11 @@ app.get("/search/:domain", async (req, res) => {
 
   if (!domain) {
     // We don't support whatever domain was provided
-    res.status(400).json({ err: "The Domain provided is not supported." });
+    res.status(400).append("Content-Type", "application/problem+json").json({
+      type: "https://search.pulsar-edit.dev/problems/unsupported-domain",
+      status: 400,
+      title: "The Domain provided is not supported."
+    });
     return;
   }
 
@@ -61,7 +79,12 @@ app.get("/search/:domain", async (req, res) => {
       await idx.load();
     } catch(err) {
       console.error(err);
-      res.status(500).json({ err: err.toString() });
+      res.status(500).append("Content-Type", "application/problem+json").json({
+        type: "about:blank",
+        status: 500,
+        title: "An error occurred when attempting to load the needed search index.",
+        detail: err.toString()
+      });
       return;
     }
 
@@ -75,13 +98,22 @@ app.get("/search/:domain", async (req, res) => {
 
 app.use(async (req, res) => {
   // 404 error
-  res.status(404).json({ message: "Not Found" });
+  res.status(404).append("Content-Type", "application/problem+json").json({
+    type: "about:blank",
+    status: 404,
+    title: "Not Found"
+  });
 });
 
 app.use((err, req, res, _next) => {
   // Global error handler, for internal logic & ExpressJS errors
   console.error(err);
-  res.status(500).json({ err: err.toString() });
+  res.status(500).append("Content-Type", "application/problem+json").json({
+    type: "about:blank",
+    status: 500,
+    title: "An error occurred when attempting to respond to your request.",
+    detail: err.toString()
+  });
 });
 
 app.listen(port, () => {
